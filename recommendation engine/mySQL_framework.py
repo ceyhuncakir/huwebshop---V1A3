38d2620f162dd7c898576b3d715e635b7bf13da6
GMKenny <kenny.vandenberg@student.hu.nl>
2021-04-07 15:31:55 +0200

    Main sql framework update
    Functie: Voor het maken van de table in mySQL database

--- recommendation engine/mySQL_framework.py
+++ recommendation engine/mySQL_framework.py
@@ -24,3 +24,19 @@ def sql_closer(db, cursor):
     cursor.close()
     db.commit()
     db.close()
+
+
+def create_rule_table(cursor, table_name, column, direction):
+    """
+    Create table with name variable "table_name" for to hold the recommendations.
+    :param cursor:
+    :param table_name:
+    """
+    # Create a table if it not already exists
+    if direction == 1:
+        query = "CREATE TABLE IF NOT EXISTS %s (id VARCHAR(255) PRIMARY KEY UNIQUE, product_1 VARCHAR(255)" \
+               ", product_2 VARCHAR(255), product_3 VARCHAR(255), product_4 VARCHAR(255))""" % table_name
+    elif direction == 2:
+        query = "CREATE TABLE IF NOT EXISTS %s (id VARCHAR(255) PRIMARY KEY UNIQUE, %s VARCHAR(255))" % (table_name, column)
+
+    cursor.execute(query)
