a943a63ac186e993de9ef2d42e5439bb5ef10eee
IzabelleVi <izabelle.auriaux@student.hu.nl>
2021-04-07 16:02:06 +0200

    Interface input
    Functie om de input op te halen en de verschillende recommedations aan te roepen.

--- recommendation engine/Interface.py
+++ recommendation engine/Interface.py
@@ -31,3 +31,32 @@ def banner():
     ''', colors[randrange(5)]))
 
 
+def interface():
+    """
+    interface functie waar de gebruiker input kan hebben
+    :return
+    """
+
+    banner()
+    login_dict = {}
+
+    login_dict['host'] = input(colored("\n\t" + "Geef je host op: ", "yellow"))
+    login_dict['gebruiker'] = input(colored("\n\t" + "Geef je mysql username op: ", "yellow"))
+    login_dict['wachtwoord'] = input(colored("\n\t" + "Geef je mysql password op: ", "yellow"))
+    login_dict["dbNaam"] = input(colored("\n\t" + "Geef je mysql database op: ", "yellow"))
+
+    end_1, start_1 = recommendation_gender(login_dict)
+    print(colored("\n\t recommendation_gender finished!", "green"))
+    end_2, start_2 = recommendation_subcategory(login_dict)
+    print(colored("\n\t recommendation_subcategory finished!", "green"))
+    end_3, start_3 = recommendation_profile_orders_process(login_dict)
+    print(colored("\n\t recommendation_profile_order finished!", "green"))
+    end_4, start_4 = recommendation_main_category(login_dict)
+    print(colored("\n\t recommendation_main_category finished!", "green"))
+
+    totaaltime = (end_1 - start_1) + (end_2 - start_2) + (end_3 - start_3) (end_4 - start_4)
+
+    print(colored("\n\tTotal time taken, " + str(round(end_3 - start_3, 6)) + " seconds", "green"))
+
+
+interface()
